6cc5220e81d60d407a17ad7f48cd5a9d816a4439
sanghyun.lee <sanghyun.lee@deleo.co.kr>
2024-11-26 12:58:47 +0900

    day1 오전 수업

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -25,3 +25,60 @@ class FirstClass {
 
 let firstClass = FirstClass(name: "John")
 firstClass.displayInfo()
+
+dump(firstClass)
+
+let sum : Int
+
+sum = 3
+
+print(sum)
+
+//  data 타입
+var aChar: Character = "🇰🇷"
+print(aChar)
+
+//var someAny: Any = 100 tClass(name: "John")
+
+//print(sommObject)
+
+/*
+ Collection
+ Array, Dictionary, Set
+*/
+
+var array: [Int] = [1, 2, 3, 4, 5]
+var intArray : Array<Int> = Array<Int>()
+
+intArray.append(6)
+
+print(intArray)
+
+var someDictionary: [String: Int] = [:]
+var someDictionary2: Dictionary<String , Int> = [:]
+someDictionary["key"] = 100
+someDictionary["key2"] = 200
+
+print(someDictionary)
+
+someDictionary.removeValue(forKey: "key")
+
+print(someDictionary)
+
+/*
+ 함수
+ */
+func sum(_ a: Float, _ b: Float) -> Float {
+    return a + b
+}
+func sum(first a: Int, second b: Int) -> Int {
+    return a + b
+}
+print(sum(first: sum(first: 1, second: 2), second:sum(first:3, second:4)))
+
+/*
+ 데이터 타입 함수
+ */
+var sumVariable : (Int, Int) -> Int = sum(first: second:)
+
+print("\(sumVariable(1, 2))")
