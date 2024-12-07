6f741d8cd590ef20bdc03f98a005349d390f3b65
sanghyun.lee <sanghyun.lee@deleo.co.kr>
2024-12-07 09:17:15 +0900

    따라하는 IOS 프로젝트

---
+++ UpDownGame/UpDownGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UpDownGame
+//
+//  Created by lee sang hyun on 12/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UpDownGame/UpDownGameTests/UpDownGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  UpDownGameTests.swift
+//  UpDownGameTests
+//
+//  Created by lee sang hyun on 12/7/24.
+//
+
+import Testing
+@testable import UpDownGame
+
+struct UpDownGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
