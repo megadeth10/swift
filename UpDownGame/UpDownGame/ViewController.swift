f4a8beae18cd87a789abb3aa183eb0c37c5dd63d
sanghyun.lee <sanghyun.lee@deleo.co.kr>
2024-12-13 22:00:48 +0900

    간단한 UI 컴포넌트와 액션 연결 학습

--- UpDownGame/UpDownGame/ViewController.swift
+++ UpDownGame/UpDownGame/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var currentSliderValue: UILabel!
 
+    private let sliderDefaultValue: Float = 15
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func valueChanged(_ sender: UISlider) {
+        print(sender.value)
+        currentSliderValue.text = String(Float(sender.value))
+    }
+    
+    @IBAction func touchInHitButton(_ sender: UIButton) {
+        print(slider.value)
+    }
+    
+    @IBAction func touchResetButton(_ sender: UIButton) {
+        slider.value = sliderDefaultValue
+    }
 }
 
